bd39755a8f1af2be91e3e025fe412a0d38c677b2
João Victor Barbosa <87194640+BarbosaXBarbosa@users.noreply.github.com>
2022-09-15 21:41:40 -0300

    Adicionando o Data Acess Object.

---
+++ ChamadosTecData/Dao/BdSqlServerDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChamadosTecData.Dao
+{
+    class BdSqlServerDao
+    {
+        //Configuração do banco de dados
+        public readonly string conexaoSqlServer = "Data Source=localhost;" +
+            " Database=ChamadosTecnicosBd;" +
+            " Integrated Security=True";
+    }
+}
