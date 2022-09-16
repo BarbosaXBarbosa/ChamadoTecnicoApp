b3b52e564a2faab2a8c5edca60039ae81810e3cf
João Victor Barbosa <87194640+BarbosaXBarbosa@users.noreply.github.com>
2022-09-15 22:07:58 -0300

    Adicionando o ObtemCliente ao Data Acess.

--- ChamadosTecData/Dao/ClienteDao.cs
+++ ChamadosTecData/Dao/ClienteDao.cs
@@ -134,5 +134,61 @@ namespace ChamadosTecData.Dao
                 conexao.Close();
             }
         }
+
+        public Clientes ObtemCliente(int codigoCliente)
+        {
+            //Instanciar a conexão
+            SqlConnection conexao = new SqlConnection();
+            //Configurar a conexão
+            conexao.ConnectionString = conexaoSqlServer;
+
+            //Instanciar o camando
+            SqlCommand comando = new SqlCommand();
+            //Criar a instrução sql
+            string sql = "Select * From Clientes" +
+                "Where CodigoCliente = @CodigoCliente;";
+            //Setar a instrução sql no comando
+            comando.CommandText = sql;
+            //Setar o tipo de comando
+            comando.CommandType = System.Data.CommandType.Text;
+            //Preencher o os parametros do B.D com as informações do cliente
+            comando.Parameters.AddWithValue("@CodigoCliente", codigoCliente);
+            //Setar a execucação do comando na conexao com o B.D
+            comando.Connection = conexao;
+            //Tratamento de erro para execução do comando
+
+            try
+            {
+                //Connectar no B.D
+                conexao.Open();
+                //Lê os dados e transfere para a tabela em memória
+                SqlDataReader drTabela = comando.ExecuteReader();
+                //Verificar se tem dados
+                if (drTabela.Read())
+                {
+                    Clientes cliente = new Clientes();
+                    cliente.CodigoCliente = int.Parse(drTabela["CodigoCliente"].ToString());
+                    cliente.Nome = drTabela["Nome"].ToString();
+                    cliente.Profissao = drTabela["Profissao"].ToString();
+                    cliente.Setor = drTabela["Setor"].ToString();
+                    
+                    return cliente;
+                }
+
+                return null;
+
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                //Desconectar do B.D
+                conexao.Close();
+            }
+
+
+        }
     }
 }
