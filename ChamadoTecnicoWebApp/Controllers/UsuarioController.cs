3b70d822b373af3817d57565b7cf0d7c4b1d3cdc
João Victor Barbosa <87194640+BarbosaXBarbosa@users.noreply.github.com>
2022-10-03 11:09:06 -0300

    Atualização da versão: Aula 9.

---
+++ ChamadoTecnicoWebApp/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChamadoTecnicoWebApp.Models;
+
+namespace ChamadoTecnicoWebApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            //Instancia o model com os dados 
+            LoginViewModel loginVM = new LoginViewModel();
+            //Envia o model para a view(tela)
+            return View(loginVM);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro()
+        {
+            //Instancia o model com os dados
+            UsuarioViewModel usuarioVm = new UsuarioViewModel();
+            return View(usuarioVm);
+        }
+
+
+
+    }
+}
---
+++ ChamadoTecnicoWebApp/Models/UsuarioViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChamadoTecnicoWebApp.Models
+{
+    public class UsuarioViewModel
+    {
+        [Display(Name = "Código")]
+        public int CodigoUsuario { get; set; }
+
+        [Display(Name = "E-mail")] //Exibição do campo
+        [Required(ErrorMessage = "Peenchimento obrigatório!")] //Preenchimento do campo obrigatória
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail inválido!")] //Define o tipo de campo com o formato de dado específico
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Peenchimento obrigatório!")] //Preenchimento do campo obrigatória
+        [DataType(DataType.Password, ErrorMessage = "Senha inválida!")] //Define o tipo de campo com o formato de dado específico
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Informe uma senha com no mínimo 8 digitos")] //Define o tamanho do campo
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Peenchimento obrigatório!")] //Preenchimento do campo obrigatória
+        public string Perfil { get; set; }
+
+        public UsuarioViewModel()
+        {
+            CodigoUsuario = 0;
+        }
+    }
+}
