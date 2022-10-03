da7fd8ed6e1af6fa58f0d4061adde6149111a7b8
João Victor Barbosa <87194640+BarbosaXBarbosa@users.noreply.github.com>
2022-10-03 10:47:39 -0300

    Atualização da versão: Aula 4.

--- ChamadoTecnicoData/Dao/ClienteDao.cs
+++ ChamadoTecnicoData/Dao/ClienteDao.cs
@@ -52,5 +52,49 @@ namespace ChamadoTecnicoData.Dao
             }
 
         }
+
+        public void AlteraCliente(Cliente cliente)
+        {
+            //Instanciar a conexão
+            SqlConnection conexao = new SqlConnection();
+            //Configurar a conexão
+            conexao.ConnectionString = conexaoSqlServer;
+
+            //Instanciar o camando
+            SqlCommand comando = new SqlCommand();
+            //Criar a instrução sql
+            string sql = "Update Clientes Set " +
+                "Nome = @Nome, Profissao = @Profissao, Setor = @Setor) " +
+                "Where CodigoCliente = @CodigoCliente;";
+            //Setar a instrução sql no comando
+            comando.CommandText = sql;
+            //Setar o tipo de comando
+            comando.CommandType = System.Data.CommandType.Text;
+            //Preencher o os parametros do B.D com as informações do cliente
+            comando.Parameters.AddWithValue("@CodigoCliente", cliente.CodigoCliente);
+            comando.Parameters.AddWithValue("@Nome", cliente.Nome);
+            comando.Parameters.AddWithValue("@Profissao", cliente.Profissao);
+            comando.Parameters.AddWithValue("@Setor", cliente.Setor);
+            //Setar a execucação do comando na conexao com o B.D
+            comando.Connection = conexao;
+            //Tratamento de erro para execução do comando
+            try
+            {
+                //Conectar no B.D
+                conexao.Open();
+                //Executar o comando no B.D
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                //Desconectar do B.D
+                conexao.Close();
+            }
+
+        }
     }
 }
