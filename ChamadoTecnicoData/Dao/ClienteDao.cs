d9aa987c8a4345559e11aa81da6142d1065731ac
João Victor Barbosa <87194640+BarbosaXBarbosa@users.noreply.github.com>
2022-10-03 10:50:27 -0300

    Atualização da versão: Aula 5.

--- ChamadoTecnicoData/Dao/ClienteDao.cs
+++ ChamadoTecnicoData/Dao/ClienteDao.cs
@@ -64,7 +64,7 @@ namespace ChamadoTecnicoData.Dao
             SqlCommand comando = new SqlCommand();
             //Criar a instrução sql
             string sql = "Update Clientes Set " +
-                "Nome = @Nome, Profissao = @Profissao, Setor = @Setor) " +
+                "Nome = @Nome, Profissao = @Profissao, Setor = @Setor " +
                 "Where CodigoCliente = @CodigoCliente;";
             //Setar a instrução sql no comando
             comando.CommandText = sql;
@@ -95,6 +95,98 @@ namespace ChamadoTecnicoData.Dao
                 conexao.Close();
             }
 
+        }
+
+        public void ExcluiCliente(int codigoCliente)
+        {
+            //Instanciar a conexão
+            SqlConnection conexao = new SqlConnection();
+            //Configurar a conexão
+            conexao.ConnectionString = conexaoSqlServer;
+
+            //Instanciar o camando
+            SqlCommand comando = new SqlCommand();
+            //Criar a instrução sql
+            string sql = "Delete From Clientes " +
+                "Where CodigoCliente = @CodigoCliente;";
+            //Setar a instrução sql no comando
+            comando.CommandText = sql;
+            //Setar o tipo de comando
+            comando.CommandType = System.Data.CommandType.Text;
+            //Preencher o os parametros do B.D com as informações do cliente
+            comando.Parameters.AddWithValue("@CodigoCliente", codigoCliente);
+
+            //Setar a execucação do comando na conexao com o B.D
+            comando.Connection = conexao;
+            //Tratamento de erro para execução do comando
+            try
+            {
+                //Conectar no B.D
+                conexao.Open();
+                //Executar o comando no B.D
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                //Desconectar do B.D
+                conexao.Close();
+            }
+        }
+
+        public Cliente ObtemCliente(int codigoCliente)
+        {
+            //Instanciar a conexão
+            SqlConnection conexao = new SqlConnection();
+            //Configurar a conexão
+            conexao.ConnectionString = conexaoSqlServer;
+
+            //Instanciar o camando
+            SqlCommand comando = new SqlCommand();
+            //Criar a instrução sql
+            string sql = "Select * From Clientes Where CodigoCliente=@CodigoCliente;";
+            //Setar a instrução sql no comando
+            comando.CommandText = sql;
+            //Setar o tipo de comando
+            comando.CommandType = System.Data.CommandType.Text;
+            //Preencher o os parametros do B.D com as informações do cliente
+            comando.Parameters.AddWithValue("@CodigoCliente", codigoCliente);
+
+            //Setar a execucação do comando na conexao com o B.D
+            comando.Connection = conexao;
+            //Tratamento de erro para execução do comando
+            try
+            {
+                //Conectar no B.D
+                conexao.Open();
+                //Ler os dados da tabela do B.D e transferir para a memoria
+                SqlDataReader drTabela = comando.ExecuteReader();
+                //Verificar se tem dados
+                if (drTabela.Read())
+                {
+                    Cliente cliente = new Cliente();
+                    cliente.CodigoCliente = int.Parse(drTabela["CodigoCliente"].ToString());
+                    cliente.Nome = drTabela["Nome"].ToString();
+                    cliente.Profissao = drTabela["Profissao"].ToString();
+                    cliente.Setor = drTabela["Setor"].ToString();
+
+                    return cliente;
+                }
+
+                return null;
+            }
+            catch (Exception erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                //Desconectar do B.D
+                conexao.Close();
+            }
         }
     }
 }
